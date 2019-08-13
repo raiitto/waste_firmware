f8f057f9b0a0708a9b6371fafbda628f6b665514
Railson Ferreira <ralsosouza1@gmail.com>
2019-08-13 12:27:00 -0300

    Removendo arquivos

--- m17.h
+++
@@ -1,8 +0,0 @@
-
-void m17(char* parametros){
-  /*
-  for(int x=0;x<zPlan;x++){
-    eixoZ(true);
-  }
-  */
-}
--- m18.h
+++
@@ -1,7 +0,0 @@
-void m18(char* parametros){
-  /*
-  for(int x=0;x<zPlan+10;x++){
-    eixoZ(false);
-  }
-  */
-}
